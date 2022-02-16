8c7bb5ff6dbf09cfb4d6860be524c61d42f06f94
Victor Shermon <victor804.gt@gmail.com>
2022-02-16 16:17:44 -0300

    ao clicar no quizzCard outros elementos fica invisivel

--- app/_js/main.js
+++ app/_js/main.js
@@ -1,4 +1,22 @@
-import { TemplateCardQuizz } from "./templateCardQuizz.js";
+/*
+/* Template CardQuizz
+*/
+
+const TemplateCardQuizz = (id, title, figure) => {
+  return `
+    <li data-id="${id}" onclick="openedQuizz(this)">
+      <figure class="figure-quizz">
+        <img class="figure-img" src="${figure}"
+          alt="simpson" />
+        <div class="figure-gradient">
+        </div>
+        <figcaption class="quizz-title">${title}
+        </figcaption>
+      </figure>
+    </li>
+  `
+}
+
 
 /* Função de Consulta do Tipo GET */
 
@@ -8,16 +26,33 @@ const  queryGetApi = async (router) => {
   return queryResponseJson
 }
 
+/* Função selectElement seleciona elementos html */
+
 const selectElement = (element, type) => {
   return (type === 'all') ? document.querySelectorAll(element) : document.querySelector(element);
 }
+
+/* Função renderCardQuizzScreen renderiza elementos na tela */
 
 const renderCardQuizzScreen = async () =>  {
   const quizzContainer = selectElement('.list-quizz-area > .list-quizz > ul', 'single')
   const quizzes = await queryGetApi('quizzes')
   quizzes.forEach( item => {
-    quizzContainer.innerHTML += TemplateCardQuizz(item.title, item.image)
+    quizzContainer.innerHTML += TemplateCardQuizz(item.id, item.title, item.image)
   } )
+}
+
+/*
+/* No momento do click do quiz a função openedQuizz e chamada colocando o
+*/
+
+function openedQuizz(elementQuizz) {
+ const sectionListQuizzToInvisible = selectElement('.add-quizz', 'single')
+ makeElementInivisble(sectionListQuizzToInvisible)
+}
+
+const makeElementInivisble = (element) => {
+  element.style.display = "none"
 }
 
 renderCardQuizzScreen()
